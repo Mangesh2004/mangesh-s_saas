398feb4187745c138563a93578910121e09d9dab
Mangesh2004 <mangeshwaybhase01@gmail.com>
2024-08-25 23:49:17 +0530

    updtae

--- app/dashboard/content/[templete-slug]/page.tsx
+++ app/dashboard/content/[templete-slug]/page.tsx
@@ -13,57 +13,50 @@ import { schema } from '@/utils/schema'
 import { useUser } from '@clerk/clerk-react'
 import moment from 'moment';
 
-
-interface PROPS{
-    params:{
-        'templete-slug':string
+interface PROPS {
+    params: {
+        'templete-slug': string
     }
 }
 
-export default function CreateNewContent(props:PROPS) {
-    const selectedTemplete:TEMPLETE|undefined=Templete?.find((item)=>item.slug==props.params['templete-slug'])
-    const [loading, setloading]=useState(false)
-    const {user}=useUser()
-    const [AiOutput, setAiOutput]=useState<string>()
-    const GenerateAIcontent=async (formdata:any)=>{
-      setloading(true)
-      const selectedPrompt=selectedTemplete?.aiPrompt
-      const finalPrompt=JSON.stringify(formdata)+", "+selectedPrompt;
-      const result=await chatSession.sendMessage(finalPrompt)
-      setAiOutput(result?.response.text())
-      await SaveinDB(formdata,selectedTemplete?.slug,result?.response.text())
-      setloading(false)
+export default function CreateNewContent({ params }: PROPS) {
+    const selectedTemplete: TEMPLETE | undefined = Templete?.find((item) => item.slug === params['templete-slug'])
+    const [loading, setLoading] = useState(false)
+    const { user } = useUser()
+    const [AiOutput, setAiOutput] = useState<string>()
+
+    const GenerateAIcontent = async (formData: any) => {
+        setLoading(true)
+        const selectedPrompt = selectedTemplete?.aiPrompt
+        const finalPrompt = JSON.stringify(formData) + ", " + selectedPrompt;
+        const result = await chatSession.sendMessage(finalPrompt)
+        setAiOutput(result?.response.text())
+        await SaveInDB(formData, selectedTemplete?.slug, result?.response.text())
+        setLoading(false)
     }
 
-    const SaveinDB=async(formdata:any, slug:any, AiOutput:string)=>{
-      const result=await db.insert(schema).values({
-        formdata:formdata,
-        templeteSlug:slug,
-        aiResponse:AiOutput,
-        createdBy: user?.primaryEmailAddress?.emailAddress || '',
-        createdAt: moment().format('DD/MM/YYYY')
-
-      })
-      console.log(result);
-      
+    const SaveInDB = async (formData: any, slug: any, AiOutput: string) => {
+        const result = await db.insert(schema).values({
+            formdata: formData,
+            templeteSlug: slug,
+            aiResponse: AiOutput,
+            createdBy: user?.primaryEmailAddress?.emailAddress || '',
+            createdAt: moment().format('DD/MM/YYYY')
+        })
+        console.log(result);
     }
 
-
-  return (
-    <div className='p-5 '>
-      <Link href={"/dashboard"}>
-      <Button> <ArrowLeft/> Back </Button>
-      </Link>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-5 pt-5'>
-      <FormSection selectedTemplete={selectedTemplete} loading={loading} userForminput={(v:any)=>GenerateAIcontent(v)
-      }/>
-      <div className='col-span-2'>
-      <OutputSection AiOutput={AiOutput || ""} />
-      </div>
-    </div>
-    </div>
-  )
-}
-export async function generateStaticParams() {
-  return [];
+    return (
+        <div className='p-5 '>
+            <Link href={"/dashboard"}>
+                <Button><ArrowLeft /> Back</Button>
+            </Link>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-5 pt-5'>
+                <FormSection selectedTemplete={selectedTemplete} loading={loading} userForminput={(v: any) => GenerateAIcontent(v)} />
+                <div className='col-span-2'>
+                    <OutputSection AiOutput={AiOutput || ""} />
+                </div>
+            </div>
+        </div>
+    )
 }
