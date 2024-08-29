4627699e869d01392e38484b2d7d9a9e7ebed7a0
Mangesh2004 <mangeshwaybhase01@gmail.com>
2024-08-29 16:32:23 +0530

    update db url

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://neondb_owner:etsH48rNcOpg@ep-lucky-base-a5yv9m21.us-east-2.aws.neon.tech/neondb?sslmode=require',
+      url: NEXT_PUBLIC_DB_URL,
     }
   };
